bc70df1e2e690b6feef7819c35df419e977fe5fb
shaikot07 <saifulislamshaikat007@gmail.com>
2024-05-31 21:08:26 +0600

    updated ui

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-      <h1 className="text-4xl text-center text-orange-500">This is a The Dragon News Project Main Home pages</h1>
+
+      <div className="w-6xl mx-auto">
+        <h1 className="text-4xl text-center text-orange-500">This is a The Dragon News Project Main Home pages</h1>
+        <h3 className="text-2xl text-purple-800">populer news... </h3>
+      </div>
     </main>
   );
 }
