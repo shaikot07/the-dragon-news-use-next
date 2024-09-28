9c3b66f7cc3dc8ecd50b7eee430cc37ea34b5f2c
shaikot07 <saifulislamshaikat007@gmail.com>
2024-09-28 21:02:47 +0600

    explore global error pages in next.js

---
+++ src/app/error.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const error = () => {
+    return (
+        <div>
+            this is global error pages .......
+        </div>
+    );
+};
+
+export default error;
